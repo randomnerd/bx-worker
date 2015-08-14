4ba9af6f9f45713bcb13d2280a852cdedbc1f06d
Random Nerd <randomnerd@sickoffice.net>
2015-08-14 15:49:16 +0300

    recursive RPC transactions reader

--- processor/workers/wallet.js
+++ processor/workers/wallet.js
@@ -1,6 +1,7 @@
 import bitcoin from 'bitcoin'
 import {BaseWorker} from './base'
 import Random from 'meteor-random'
+import Big from 'big.js'
 
 export class WalletWorker extends BaseWorker {
   init() {
@@ -12,6 +13,8 @@ export class WalletWorker extends BaseWorker {
   startClient(id, config) {
     this.logger.info('Starting', config.name, 'client')
     let client = this.clients[id] = new bitcoin.Client(config.rpc);
+    client.currId = id;
+    client.currName = config.name;
     client.getBalance((err, balance) => {
       this.logger.info(config.name, 'balance:', balance)
     })
@@ -52,7 +55,8 @@ export class WalletWorker extends BaseWorker {
   getJobMap() {
     return {
       testjob: this.testjob,
-      newAddress: this.newAddress
+      newAddress: this.newAddress,
+      processDeposits: this.processDeposits
     }
   }
 
@@ -100,4 +104,61 @@ export class WalletWorker extends BaseWorker {
     wallet.save(cb.bind(this));
     return wallet;
   }
+
+  processDeposits(job, callback) {
+    let curr = this.config.currencies[job.data.currId];
+    if (!curr) {
+      job.fail('Currency not configured');
+      return callback();
+    }
+
+    this.logger.info('Processing deposits for', curr.name);
+    this._processDeposits(job.data.currId);
+    job.done();
+    callback();
+  }
+
+  _processDeposits(id, skip = 0, batch = 50) {
+    let client = this.clients[id];
+
+    client.listTransactions(null, batch, skip, (err, result) => {
+      if (err) throw 'Error listing transactions: ' + err;
+      if (!result.transactions || result.transactions.length === 0) return;
+      for (let tx of result.transactions) {
+        if (tx.category !== 'receive') continue;
+        this._processDepositTx(client, tx);
+      }
+      if (result.transactions.length < batch) return;
+      this._processDeposits(id, skip + batch, batch);
+    })
+  }
+
+  _processDepositTx(client, tx) {
+    client.getTransaction(tx.txid, (err, result) => {
+      let found = 0;
+      for (let rawtx of result.details) {
+        if (rawtx.category !== 'receive') continue;
+
+        this.Wallet.findOne({address: rawtx.address}, (err, wallet) => {
+          if (!wallet) return;
+          found += 1;
+
+          this._saveDeposit(tx, rawtx, wallet)
+        })
+      }
+      if (found === 0) {
+        // something went wrong, no user wallets found for TX
+        // TODO: do something with such TXs: alert admins, etc...
+        this.logger.error(
+          client.currName, '| amount:', tx.amount, '| txid: ', tx.txid,
+          '| transaction did not land into user wallet'
+        )
+      }
+    })
+  }
+
+  _saveDeposit(tx, rawtx, wallet) {
+    // save deposit
+  }
+
 }
