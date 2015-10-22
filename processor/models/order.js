37f834bb25db78be5a2a130bfa4d427aa86f17d9
Random Nerd <randomnerd@sickoffice.net>
2015-10-22 20:55:17 +0300

    try fix

--- processor/models/order.js
+++ processor/models/order.js
@@ -32,9 +32,10 @@ OrderSchema.methods = {
       if (!matches.length) { console.log('no matches found'); return; };
 
       async.mapSeries(matches, (item, callback) => {
+        console.log('processing', item);
         this.processMatch(item, callback);
       }, (err) => {
-
+        console.log('all matches processed');
       })
     })
   },
