9dabc77d57958b08382d9fb35130e0604066f630
Random Nerd <randomnerd@sickoffice.net>
2016-08-04 19:51:17 +0300

    eth withdrawal

--- processor/models/crypto_client.js
+++ processor/models/crypto_client.js
@@ -1,12 +1,14 @@
 import Web3 from 'web3';
 import Bitcoin from 'bitcoin';
 import KeyStore from 'node-ethereumjs-keystore';
+import {Currency} from './currency';
 import {Wallet} from './wallet';
 import {Transaction} from './transaction';
 import {Withdrawal} from './withdrawal';
 import {Balance, Long} from './balance';
 import {BalanceChange} from './balance_change';
 import {Setting} from './setting';
+import Tx from 'ethereumjs-tx';
 import logger from '../logger';
 
 export default class CryptoClient {
@@ -14,14 +16,15 @@ export default class CryptoClient {
     this.currId   = id;
     this.config   = config;
     this.type     = config.type;
-    this.confReq  = config.confReq;
     this.currName = config.name;
     this.netFee   = config.netFee || 0;
     if (this.type === 'eth') {
       this.ethKeyStore = new KeyStore();
       this.ethFilter = null;
-      this.gas = config.gas;
+      this.hotAddress = config.hotAddress;
+      this.hotSecret = config.hotSecret;
       this.gasPrice = config.gasPrice;
+      this.gasLimit = config.gasLimit;
     }
     this.initClient();
   }
@@ -56,50 +59,72 @@ export default class CryptoClient {
   }
 
   moveDeposits(blockNumber) {
-    logger.info('moveDeposits', blockNumber);
     Transaction.aggregate([
       {
         $match: {
           currId: this.currId,
-          moved: { $ne: true },
-          balanceChangeId: { $ne: null },
-          amount: { $gt: this.netFee * 10 }
+          moved: false,
+          balanceChangeId: { $ne: null }
         }
       },
       {
         $group: {
           _id: "$walletId",
+          txIds: { $push: "$_id"},
           amount: { $sum: "$amount" }
         }
       }
     ]).then((items) => {
-      logger.info('moveDeposits.aggregate', items);
       for (let item of items) {
+        if (item.amount < 0.005 * Math.pow(10, 8)) continue;
         Wallet.findOne({_id: item._id}).then((wallet) => {
           let {address, secret} = wallet;
-          this.ethKeyStore.importKey(secret);
-          let gas = Long.fromNumber(this.gas);
-          let gasPrice = Long.fromNumber(this.gasPrice);
-          let fee = gas.mul(gasPrice);
-          let rawTx = {
-            to:   this.hotAddress,
-            from: address,
-            gas:  this.gas.toString(16),
-            gasPrice: this.gasPrice.toString(16),
-            amount: item.amount.subtract(fee)
-          }
-          let signedTx = this.ethKeyStore.signTransaction(txParams);
-          this._client.eth.sendRawTransaction(signedTx, (error, hash) => {
-            if (error) return logger.error("moveDeposit", error);
-            logger.info(`Stashed ${item.amount.toNumber()/Math.pow(10,8)} from ${address} to the hot wallet`);
-          })
+          let balance = this._client.eth.getBalance(address);
+          this.ethSend(address, secret, this.hotAddress, balance, (error) => {
+            if (error) return logger.error('moveDeposit', error);
+            Transaction.update({ _id: { $in: item.txIds }}, { $set: { moved: true } }, { multi: true });
+          });
         });
       }
     });
   }
 
+  ethSendFromHotWallet(to, amount, callback) {
+    if (to.match(/0x/)) to = to.slice(2, to.length);
+    this.ethSend(this.hotAddress, this.hotSecret, to, amount, callback);
+  }
+
+  ethSend(address, secret, to, amount, callback) {
+    let gasLimit = Long.fromNumber(this.gasLimit);
+    let gasPrice = Long.fromNumber(this.gasPrice);
+    let fee = gasLimit.multiply(gasPrice);
+    let nonce = this._client.eth.getTransactionCount(address);
+    let value = amount - fee.toNumber();
+    let tx = new Tx({
+      to:   '0x' + to,
+      nonce: '0x' + nonce.toString(16),
+      gasPrice: '0x' + this.gasPrice.toString(16),
+      gasLimit: '0x' + this.gasLimit.toString(16),
+      value: '0x' + value.toString(16)
+
+    });
+    tx.sign(new Buffer(secret, 'hex'));
+    this._client.eth.sendRawTransaction(tx.serialize().toString('hex'), (error, hash) => {
+      if (error) {
+        if (callback) return callback(error);
+        return logger.error(error);
+      }
+      logger.info(`Sent ${amount/Math.pow(10,18)} ${this.curr.shortName} from ${address} to ${to}`);
+      if (callback) callback(null, hash);
+    })
+  }
+
   initClient() {
     try {
+      Currency.findOne({_id: this.currId}).then((curr) => {
+        this.confReq = curr.confReq;
+        this.curr = curr;
+      });
       switch (this.type) {
         case 'eth':
           let web3 = this._client = new Web3();
@@ -154,7 +179,6 @@ export default class CryptoClient {
           break;
         default:
           this._client.getNewAddress((err, address, secret) => {
-            console.log(err, address, secret);
             callback(err, address);
           });
           break;
@@ -185,8 +209,7 @@ export default class CryptoClient {
     try {
       switch (this.type) {
         case 'eth':
-          let txid = this._client.eth.sendTransaction({to: address, value: amount});
-          txid ? callback(null, txid) : callback(new Error('unable to send transaction'));
+          this.ethSendFromHotWallet(address, amount * Math.pow(10, 18), callback)
           break;
         default:
           this._client.sendToAddress(address, amount, callback);
